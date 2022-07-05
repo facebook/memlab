2c2562069e9a216b7352f1067809ac14db9b2787
Tulga Narmandakh <tulgan@fb.com>
2022-07-05 15:47:22 -0700

    docs(guides): creating a guide page for Detached DOM example
    Summary: add guidance doc page for detecting detached dom element from example app.
    
    Reviewed By: JacksonGL
    
    Differential Revision: D37617952
    
    fbshipit-source-id: b12514d9c50895b034fb5576a356386414e48fdf

--- packages/e2e/static/example/scenarios/detached-dom.js
+++ packages/e2e/static/example/scenarios/detached-dom.js
@@ -1,8 +1,18 @@
 // @nolint
+// memlab/packages/e2e/static/example/scenario/detached-dom.js
+/**
+ * The initial `url` of the scenario we would like to run.
+ */
 function url() {
   return "http://localhost:3000/examples/detached-dom";
 }
 
+/**
+ * Specify how memlab should perform action that you want
+ * to test whether the action is causing memory leak.
+ *
+ * @param page - Puppeteer's [page object](https://pptr.dev/api/puppeteer.page/).
+ */
 async function action(page) {
   const [button] = await page.$x(
     "//button[contains(., 'Create detached DOMs')]"
@@ -12,6 +22,12 @@ async function action(page) {
   }
 }
 
+/**
+ * Specify how memlab should perform action that would
+ * reset the action you performed above.
+ *
+ * @param page - Puppeteer's [page object](https://pptr.dev/api/puppeteer.page/).
+ */
 async function back(page) {
   const [button] = await page.$x('a[href="/"]');
   if (button) {
--- packages/e2e/static/example/scenarios/event-listener.js
+++ packages/e2e/static/example/scenarios/event-listener.js
@@ -1,4 +1,6 @@
 // @nolint
+
+// memlab/packages/e2e/static/example/scenario/event-listener.js
 function url() {
   return "http://localhost:3000/";
 }
@@ -13,10 +15,8 @@ async function back(page) {
   await page.click('a[href="/"]');
 }
 
-// function leakFilter(node, heap, snapshot) {
-//   return node.references?.some(
-//     (reference) => reference.name_or_index === 'bigArray'
-//   );
-// }
+// function leakFilter(node, _snapshot, _leakedNodeIds) {
+//   return node.retainedSize > 1000 * 1000;
+// };
 
-module.exports = { action, back, url };
+module.exports = { action, back, url, leakFilter };
