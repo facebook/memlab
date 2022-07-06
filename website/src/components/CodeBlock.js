0eede294e4e0a092fbd275ce1bfad481b9307cec
Nidhi Kadkol <nidhikadkol@fb.com>
2022-07-05 18:09:35 -0700

    Add new annotation format to non-generated files (106/n)
    Summary:
    Context - [[quip](https://fb.quip.com/1T2uAglapSkF)]
    
    We are updating the oncall annotation format of all javascript files from  `emails oncall+my_oncall_shortname` to `oncall my_oncall_shortname`.
    
    NOTE: There are other linter errors in some of these files that have not been introduced by this change, they are not being fixed here since they are non-trivial fixes (eg. moving a file to a different path).
    
    Reference wiki for ESLint - [[wiki](https://www.internalfb.com/intern/wiki/Eslint/Creating_a_Lint_Rule/Writing_a_lint_rule/)]
    
    To ensure we don't break anything, we are making this update with the following steps:
    1. Update codegen to add the new annotations to all generated files
    2. Update the docblocks for mock static JS generated files
    3. Add new linter rule to add the annotation to existing non-codegen files - D36615978
    4. **(This diff)** Add the new annotation to existing non-codegen files
    5. Update all oncall extraction logic and Ownership Enumeration logic to read the new format
    6. Build linter rule to ask user to add new annotation to new files
    7. Another round of codemod to add new annotation to existing non-codegen files
    8. Remove old annotations and linter rules
    
    drop-conflicts
    allow-large-files
    
    Reviewed By: stanley-shi
    
    Differential Revision: D37541172
    
    fbshipit-source-id: 6b05879a0e6f1237a1b4a81a4d10ef7c1eb4683b

--- website/src/components/CodeBlock.js
+++ website/src/components/CodeBlock.js
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * @oncall ws_labs
  * @emails oncall+ws_labs
  * @format
  */
