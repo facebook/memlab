5f181306950bc4b6d32a1022298f3d83ed9bde6b
Rujin Cao <rujinc@fb.com>
2022-08-26 11:59:02 -0700

    @emails -> @oncall (*.ts) part 1/n
    Reviewed By: bhamodi
    
    Differential Revision: D39048295
    
    fbshipit-source-id: 1876c44b2483ec62491d084838dd41dc9ff76c1b

--- packages/heap-analysis/src/plugins/GlobalVariableAnalysis/BuiltInGlobalVariables.ts
+++ packages/heap-analysis/src/plugins/GlobalVariableAnalysis/BuiltInGlobalVariables.ts
@@ -4,9 +4,9 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @emails oncall+ws_labs
- * @lightSyntaxTransform
  * @format
+ * @lightSyntaxTransform
+ * @oncall ws_labs
  */
 
 export default new Set([
